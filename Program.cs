fb34d2f89e62ba0229b91c4aff13c167cea1d618
fluffnstuff88 <wagnercalaes@live.com>
2023-01-30 13:44:11 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("-----------TESTE NOKIA-----------");
+Smartphone nokia = new Nokia("N0K123", "MODEL NOK123", "1111111", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("FACEBOOK");
+
+System.Console.WriteLine("\n");
+System.Console.WriteLine("-----------TESTE IPHONE-----------");
+Smartphone iphone = new Iphone("I123", "MODEL IO123", "242424242", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("SIGNAL");
